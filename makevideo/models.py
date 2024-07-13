9b306cecfad6967a973d08d90c0ab6554a846af7
speedfreak <gdramze@gmail.com>
2024-07-14 01:32:49 +0300

    add save to db

--- makevideo/models.py
+++ makevideo/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 
-class Requests(models.Model):
+class Request(models.Model):
     query = models.TextField()
--- makevideo/views.py
+++ makevideo/views.py
@@ -1,5 +1,11 @@
+
 from django.shortcuts import render
+
+from makevideo.models import Request
 
 
 def index(request):
+    param = request.GET.get('text')
+    if param:
+        Request.objects.create(query=param)
     return render(request, 'makevideo/index.html')
